83ae34df872b5c49eb8f08d1927c25c26f322aa7
Aji Sathyanandhan <83906143+ajislal@users.noreply.github.com>
2024-11-13 16:00:47 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -118,5 +118,5 @@ def check_content_type(media_type):
         return account.serialize(), status.HTTP_200_OK
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
+        resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
