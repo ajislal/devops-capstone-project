87bb28be08dc32aff10cbb420ea396586c2cf19b
Aji Sathyanandhan <83906143+ajislal@users.noreply.github.com>
2024-11-13 16:56:08 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -116,7 +116,3 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-def test_get_account_not_found(self):
-    """It should not Read an Account that is not found"""
-    resp = self.client.get(BASE_URL)
-    self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
