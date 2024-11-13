df1f9ed53562e6cc65b5210f48e1001bdccdaa53
Aji Sathyanandhan <83906143+ajislal@users.noreply.github.com>
2024-11-13 16:44:48 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -1,3 +1,4 @@
+
 """
 Account Service
 
@@ -105,18 +106,18 @@ def check_content_type(media_type):
     ######################################################################
     @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-        """
-        Reads an Account
-        This endpoint will read an Account based the account_id that is requested
-        """
-        app.logger.info("Request to read an Account with id: %s", account_id)
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
 
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-        return account.serialize(), status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 def test_get_account_not_found(self):
-        """It should not Read an Account that is not found"""
-        resp = self.client.get(BASE_URL)
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    """It should not Read an Account that is not found"""
+    resp = self.client.get(BASE_URL)
+    self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
