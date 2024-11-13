93d4ea630ffb28610251b9b22c3200adadb913de
Aji Sathyanandhan <83906143+ajislal@users.noreply.github.com>
2024-11-13 17:00:25 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -110,11 +110,9 @@ def get_accounts(account_id):
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
     return account.serialize(), status.HTTP_200_OK
 def test_get_account_not_found(self):
     """It should not Read an Account that is not found"""
