87679712786cc7eb305a1d58ed924ea528797cea
Aji Sathyanandhan <83906143+ajislal@users.noreply.github.com>
2024-11-13 11:53:21 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -118,5 +118,5 @@ def get_accounts(account_id):
         return account.serialize(), status.HTTP_200_OK
 def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
+        resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
